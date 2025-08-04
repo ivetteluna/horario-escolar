890a957bd8f73bc8e8a84ff8bf053742d945ca51
Tu Nombre <tu@email.com>
2025-08-04 17:42:50 -0400

    fix: Corregir configuración de estilos de Tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,12 @@
+// NUEVO HORARIO/tailwind.config.ts
+
 import type { Config } from "tailwindcss"
 
 const config = {
   darkMode: ["class"],
   content: [
-    "./pages/**/*.{ts,tsx}",
-    "./components/**/*.{ts,tsx}",
-    "./app/**/*.{ts,tsx}",
-    "./src/**/*.{ts,tsx}",
-    "*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   prefix: "",
   theme: {
